0c5811bb11faa1faee10f6fa830e65c26bc6e3e0
Spencer Heywood <github@heywoodlh.io>
2024-05-01 09:01:44 -0600

    added kasm-debian

--- .github/workflows/crons.sh
+++ .github/workflows/crons.sh
@@ -1,3 +1,3 @@
 #!/usr/bin/env bash
 
-grep -A 1 schedule * | grep cron | cut -d':' -f2 | sed 's/"//g' | sort -u 
+grep -A 1 schedule * | grep cron | cut -d':' -f2 | sed 's/"//g' | sort -u | rev | sort | rev
