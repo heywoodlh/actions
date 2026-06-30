384672c6bbb02401e90add6c0f9ed4eb334c4574
Spencer Heywood <github@heywoodlh.io>
2026-06-29 21:18:17 -0600

    updated flakes

--- flake.nix
+++ flake.nix
@@ -11,10 +11,12 @@
       nixpkgs = nixos-configs.inputs.nixpkgs;
       pkgs = nixpkgs.legacyPackages.${system};
       tangled-sync = nixos-configs.packages.${system}.tangled-sync;
+      spindle-run = nixos-configs.packages.${system}.spindle-run;
     in {
       devShell = pkgs.mkShell {
         name = "heywoodlh.io shell";
         buildInputs = with pkgs; [
+          spindle-run
           yaml-language-server
           yamlfmt
         ];
